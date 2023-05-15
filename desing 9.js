98a03ea671af16b356e514e07dced20b1281a4d0
hg3537 <111694892+hg3537@users.noreply.github.com>
2023-05-15 18:04:44 +0300

    Add files via upload

---
+++ desing 9.js
@@ -0,0 +1,9 @@
+let icon=document.getElementsByClassName(`icon`)
+
+
+function backhide ()
+{
+    let maua = document.querySelector(`.muna`);
+    maua.style = `    display: block;`;
+    backhide();
+}
